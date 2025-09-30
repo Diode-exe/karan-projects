33ab12ec3afdaf626fae252d329e62b400d0d9f1
Diode-exe <DiodesCAER@protonmail.com>
2025-09-30 17:14:43 -0500

    Update tile-cost-calc.py

--- karanProjects/tile-cost-calc.py
+++ karanProjects/tile-cost-calc.py
@@ -3,10 +3,10 @@ import math
 print("Welcome to the tile cost calculator!")
 
 try: 
-    lengthOfRoom = int(input("Length of room? "))
-    widthOfRoom = int(input("Width of room? "))
-    lengthOfTile = int(input("Length of tile? "))
-    widthOfTile = int(input("Width of tile? "))
+    lengthOfRoom = int(input("Length of room in ft? "))
+    widthOfRoom = int(input("Width of room in ft? "))
+    lengthOfTile = int(input("Length of tile in ft? "))
+    widthOfTile = int(input("Width of tile in ft? "))
 except ValueError:
     print("One or more values was invalid! Numbers only, please")
     exit()
@@ -19,10 +19,10 @@ print(f"Tiles in length of room: {lengthOfRoomByTile}")
 
 exit() if input("Continue to calculate cost? y or n: ").lower() != "y" else None
 
-costOfTile = float(input("Cost of each tile? "))
+costOfTile = float(input("Cost of each tile in dollars? "))
 
 areaOfTileInRoom = lengthOfRoomByTile * widthOfRoomByTile
 
 priceOfAreaInRoom = areaOfTileInRoom * costOfTile
 
-print(f"Tiling the room will cost {priceOfAreaInRoom}")
+print(f"Tiling the room will cost {priceOfAreaInRoom} dollars to tile")
